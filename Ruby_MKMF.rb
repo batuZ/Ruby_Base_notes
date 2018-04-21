d52cb0406e854c1a6af006b7637c78e5d3e9b820
batu <304701204@qq.com>
2018-04-21 17:13:22 +0800

    add mkmf

---
+++ Ruby_MKMF.rb
@@ -0,0 +1,15 @@
+# Ruby_MKMF.rb
+
+# Ruby \ C++ \ C#
+
+# mkmf Ruby 调用C++函数或库，资料：
+# http://raylinn.iteye.com/blog/629329
+# https://blog.csdn.net/frodo_sens/article/details/38068623
+
+# 另：IronRuny  .net版ruby 相关资料
+# 官方：https://msdn.microsoft.com/zh-cn/magazine/dd434651.aspx
+# 非官方：http://raylinn.iteye.com/blog/626705
+# 大神的block: 爬向春天的蜗牛
+# => http://raylinn.iteye.com/category/87176
+
+require 'mkmf' 
