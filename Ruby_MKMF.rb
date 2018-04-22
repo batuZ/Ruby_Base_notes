0798e652fec56455d96507b1420a376ed5999e6d
batuZ <304701204@qq.com>
2018-04-22 21:01:36 +0800

    11

--- Ruby_MKMF.rb
+++ Ruby_MKMF.rb
@@ -5,6 +5,9 @@
 # mkmf Ruby 调用C++函数或库，资料：
 # http://raylinn.iteye.com/blog/629329
 # https://blog.csdn.net/frodo_sens/article/details/38068623
+# https://blog.csdn.net/emerald0106/article/details/7385370
+# http://www.jb51.net/article/64144.htm
+# https://ruby-china.org/topics/23030
 
 # 另：IronRuny  .net版ruby 相关资料
 # 官方：https://msdn.microsoft.com/zh-cn/magazine/dd434651.aspx
--- test.rb
+++ test.rb
@@ -1,72 +1,9 @@
-class A
-	Cre  =789
-	def initialize
-		@var = 4
-	end
-end
-#     instance_eval
-
-
-# ------------- 实例变量 O-----------------------
-	# a = A.new
-	# a.instance_eval do
-	#     def hello
-	#         puts @var
-	#     end
-	# end
-	# a.hello
-# ------------- 实例变量 O-----------------------
-	# A.class_eval do
-	#     def hello
-	#          puts @var
-	#     end
-	# end
-	# A.new.hello
-# ------------- 类变量 -----------------------
-A.instance_eval do
-    def hello
-	    puts "aaa"
-    end
-    puts "bbbb"
-end
-puts A.hello
-
-# ------------- 类变量 -----------------------
-# A.singleton_class.class_eval do
-#     def hello
-#         p "hello"
-#     end
-# end
-# A.hello
-
-
-
-
- # A.instance_exec("hellos"){|x| p "#{x}"}
-
-
-
-# 五，instance_eval()和instance_exec() 
-# 在Ruby中，
-# 提供了一个非常酷的特性，可以通过使用Objec#instance_eval()， 
-# Objec#instance_exec()方法插入一个代码块，做一个的对象上下文探针（Context Proble），
-# 深入到对象中的代码片段，对其进行操作。有了这个特性以后，就可以很轻松的测试对象的行为，查看对象的当前状态。
-
-# class MyClass 
-# 	def initialize 
-# 		@v = 1; 
-# 	end
-# end
-
-# obj = MyClass.new
-# obj.instance_eval do
-# 	puts self	# => #<MyClass:0x007fbb2d0299b0> 
-# 	puts @v 	# => 1 
-# end
-
-# obj.instance_exec(5) { |x| puts x * @v }
-# => 5 	
-
-# => https://blog.csdn.net/xluren/article/details/43964433
-# => https://blog.csdn.net/baodongdong123/article/details/75206846
-# => https://blog.csdn.net/sunset108/article/details/48136571
+require 'mkmf'
+require 'testPP'
+# Give it a name  
+# extension_name = 'testPP'  
+# The destination  
+# dir_config(extension_name)  
+# dir_config(extension_name,'C:/test/Include','C:/test/lib')  
+# Do the work  
+# create_makefile(extension_name)
